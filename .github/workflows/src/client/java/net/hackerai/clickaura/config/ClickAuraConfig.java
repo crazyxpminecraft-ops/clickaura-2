3aca2846980bba230013795a050e45ab200a0f98
crazyxpminecraft-ops <crazyxpminecraft@gmail.com>
2026-07-15 22:27:22 +0530

    Create ClickAuraConfig.java

---
+++ .github/workflows/src/client/java/net/hackerai/clickaura/config/ClickAuraConfig.java
@@ -0,0 +1,12 @@
+package net.hackerai.clickaura.config;
+
+public class ClickAuraConfig {
+    public double range = 10.0;
+    public boolean silentRotations = true;
+    public boolean autoSwing = true;
+    public boolean targetPlayers = true;
+    public boolean targetHostile = false;
+    public boolean targetPassive = false;
+    public double minCps = 8.0;
+    public double maxCps = 13.0;
+}
